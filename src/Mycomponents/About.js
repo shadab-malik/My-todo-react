d64f907beb3d2f66946f1843e44ac0217804ce66
shadab-malik <img_2020057@iiitm.ac.in>
2021-12-10 16:02:11 +0530

    updating about

--- src/Mycomponents/About.js
+++ src/Mycomponents/About.js
@@ -3,25 +3,38 @@ import React from 'react'
 export default function About() {
     return (
       <div>
-        this is about
-        <p>
-          
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Id
-          perspiciatis quos illo placeat at ipsa! Consectetur, ut! Sapiente
-          molestias nihil quibusdam eligendi doloribus soluta incidunt rerum
-          placeat temporibus iusto vitae excepturi molestiae, laboriosam,
-          impedit voluptate distinctio consectetur ad saepe alias quidem aliquid
-          animi? Quidem est magnam, sed vitae labore, perspiciatis quo minima
-          blanditiis nostrum cumque eligendi odit eius nihil molestiae expedita
-          fugiat! Obcaecati in temporibus delectus eveniet. Neque corrupti
-          voluptatum fuga delectus ducimus, eligendi at nihil vero, perferendis
-          iure eius optio pariatur cumque odio dolorem expedita corporis
-          laboriosam sequi officia fugiat sit dicta quae ad. Ex laborum nam ut?
-          Et, veniam. Perferendis in quam animi ipsum cum illum culpa nihil,
-          possimus omnis sed corrupti officia deserunt aut, aliquid praesentium
-          minus atque delectus iusto! Est nesciunt optio cupiditate quos beatae
-          eligendi similique perspiciatis sit iste commodi.
-        </p>
+        <h1 className="text-center text-primary my-3">About</h1>
+        <div className="container">
+          <img
+            className="rounded mx-auto d-block"
+            src="https://media.istockphoto.com/photos/to-do-list-picture-id504647866?k=6&m=504647866&s=170667a&w=0&h=7oKIyLAoL7zrjfXtdPYIDWO8g-Gjuz_OcQuxX4cfjQE="
+            alt="TODO"
+          />
+
+          <p className="container my-5 ">
+            ToDo List App is a kind of app that generally used to maintain our
+            day-to-day tasks or list everything that we have to do, with the
+            most important tasks at the top of the list, and the least important
+            tasks at the bottom. It is helpful in planning our daily schedules.
+            We can add more tasks at any time and delete a task that is
+            completed.
+          </p>
+          <p className="container my-5  ">
+            In this version of the ToDo list, the user will be getting 
+            <ul>
+              <li>
+                Create (add) a new task or adding a new ToDo in the ToDo List
+                App and can save them for further future use.
+              </li>
+              <li>
+                See all the tasks or View all the ToDos that were added to the
+                app.
+              </li>
+              <li>Delete any ToDo from the list of ToDos.</li>
+              <li>Exit from the app.</li>
+            </ul>
+          </p>
+        </div>
       </div>
     );
 }
